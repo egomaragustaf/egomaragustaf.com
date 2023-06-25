cab256237e64d6306e69455e38687b7e240da388
Ego Maragustaf <egomaragustaf@gmail.com>
2023-06-25 21:33:06 +0700

    add script contact form

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -1,1 +1,16 @@
-console.log("contact")
+const contactFormElement = document.getElementById("contact-form");
+const contactNameInputElement = document.getElementById("name");
+const contactEmailInputElement = document.getElementById("email");
+const contactMessageInputElement = document.getElementById("message");
+
+contactFormElement.onsubmit = function (event) {
+  event.preventDefault();
+
+  const contactNameValue = contactNameInputElement.value;
+  const contactEmailValue = contactEmailInputElement.value;
+  const contactMessagelValue = contactMessageInputElement.value;
+  
+  console.log(contactNameValue);
+  console.log(contactEmailValue);
+  console.log(contactMessagelValue);
+};
