26d2ad06502e117f9a81b0963703b7818dc07980
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-06 19:43:26 +0700

    feat: add dialog feature after send message in contact page

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -3,14 +3,24 @@ const contactNameInputElement = document.getElementById("name");
 const contactEmailInputElement = document.getElementById("email");
 const contactMessageInputElement = document.getElementById("message");
 
-contactFormElement.onsubmit = function (event) {
+contactFormElement.addEventListener("submit", submitContactForm);
+
+function submitContactForm(event) {
   event.preventDefault();
 
   const contactNameValue = contactNameInputElement.value;
   const contactEmailValue = contactEmailInputElement.value;
   const contactMessagelValue = contactMessageInputElement.value;
-  
-  console.log(contactNameValue);
-  console.log(contactEmailValue);
-  console.log(contactMessagelValue);
-};
+
+  const dialogContent = document.getElementById("dialog-content");
+  dialogContent.innerHTML = `
+      <h1>Hello ${contactNameValue}</h1>
+      <h2>Thanks for sending me message with your email ${contactEmailValue}</h2>
+      <p>I really apreciate it! 🙏</p>
+    `;
+
+  const dialog = document.getElementById("dialog");
+  dialog.showModal();
+
+  console.log("test");
+}
