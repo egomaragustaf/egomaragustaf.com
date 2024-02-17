4ca66e25cb1e145da0b7041bb9f3ebd637e808fb
Ego Maragustaf <egomaragustaf@gmail.com>
2024-02-17 22:40:10 +0700

    chore: init shadcn-ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Ego Maragustaf" },
+    { name: "description", content: "Welcome to egomaragustaf.com!" },
   ];
 };
 
@@ -11,12 +11,10 @@ export default function Index() {
   return (
     <div className="flex justify-center items-center h-screen">
       <main className="w-full max-w-3xl text-center space-y-2">
-        <h1 className="text-xl">Welcome to My Website</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla minima
-          alias nostrum veritatis qui sapiente velit rem eaque, animi ad ipsa
-          laboriosam autem, facilis deserunt quo, rerum aliquam ab doloribus?
-        </p>
+        <h1 className="text-xl">Hi!</h1>
+        <h2>I'm Ego Maragustaf</h2>
+        <h3>Frontend Developer</h3>
+        <p>I design beautifuly simple things, and love what I do</p>
       </main>
     </div>
   );
