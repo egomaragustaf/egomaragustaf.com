8367601698b31593ef21db52a9e44889019e4a96
egomaragustaf <egomaragustaf@gmail.com>
2026-03-27 21:29:42 +0700

    feat: migrate to nextjs

--- dist/js/script.js
+++
@@ -1,9 +0,0 @@
-// hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-}
-)
