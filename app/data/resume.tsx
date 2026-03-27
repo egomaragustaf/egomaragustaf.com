2aad2777d06b0658017298fd81faee73eec7c724
egomaragustaf <egomaragustaf@gmail.com>
2026-03-27 23:52:27 +0700

    feat: hero section

--- app/data/resume.tsx
+++ app/data/resume.tsx
@@ -2,6 +2,11 @@ import { Icons } from "@/components/icon";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
+  name: "Ego Maragustaf",
+  initials: "EGM",
+  url: "https://egomaragustaf.com",
+  description:
+    "Software Engineer that love building things and helping people.",
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
+import BlurFadeText from "@/components/blur-fade-text";
+import { BLUR_FADE_DELAY } from "./config/config-ui";
+import { DATA } from "./data/resume";
+import BlurFade from "@/components/blur-fade";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Ego Maragustaf
-        </h1>
-        <p>Software Engineer</p>
-      </main>
-    </div>
+    <main className="min-h-dvh flex flex-col gap-14 relative">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 gap-y-6 flex flex-col md:flex-row justify-between">
+            <div className="gap-2 flex flex-col order-2 md:order-1">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-semibold tracking-tighter sm:text-4xl lg:text-5xl"
+                yOffset={8}
+                text={`Hello 👋, I'm ${DATA.name.split(" ")[0]}`}
+              />
+              <BlurFadeText
+                className="text-muted-foreground max-w-150 md:text-lg lg:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY} className="order-1 md:order-2">
+              <Avatar className="size-24 md:size-32 border rounded-full shadow-lg ring-4 ring-muted">
+                {/* <AvatarImage alt={DATA.name} src={DATA.avatarUrl} /> */}
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
